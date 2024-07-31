4df41ede990dcd121ae4cc2665db3a7f705aa573
EmanGurung <136867625+winterwithouther@users.noreply.github.com>
2024-07-31 16:07:28 -0400

    adding tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +20,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
+        <p className='text-green-200'>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
